b9cd8241e96e3f2804f72ec9857af17ddc35d821
Bree Warren <ambreea.warren@gmail.com>
2020-02-28 00:35:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,7 @@
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  p "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  def second_sentence = "A plate of #{protein} with #{veg1} and #{veg2}."
 end
+
+meal_choice(second_sentence)
